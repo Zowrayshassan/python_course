6c8eb0287c958390146c65a9ce8c933976049708
Zowrays Hassan <zowrayshassan@gmail.com>
2026-02-07 15:47:17 +0500

    lec 3

---
+++ extra.py
@@ -0,0 +1,38 @@
+
+#we use loops to repeat the task
+
+#for loop and while loop 
+
+#while(which we use when dont know the condition)
+
+
+#print a number from 0 to 10 / 1 to 10
+# i=0
+# while(i<=10):
+#     print(i)
+#     i=i+1
+    
+# #print a number from 10 to 1
+# i=10
+# while(i>=1):
+#     print(i)
+#     i=i-1
+    
+    
+# #print even number from 1 to 20 :hint(increment by 2)
+# i=0
+# while(i<=20):
+#     print(i)
+#     i=i+2
+    
+    
+#print sum of first 10 numbers hint:have to make extra variable for sum=0
+
+
+sum=0
+i=1
+while(i<=10):
+    sum=sum+i
+    i=i+1       
+    print('the value of i is', i)
+    print("The sum of first 10 numbers is: ", sum)
